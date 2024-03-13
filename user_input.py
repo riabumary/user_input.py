981a22e3ebcd122d255686f3d72fc48ce599eace
riabumary <riabumary81@gmail.com>
2024-03-13 12:33:27 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input ("what is your name?")
+age = input ("what is your age?")
+location = input("whats your location?")
+
+print(f"hello {name},you are {age}years old and live at{location}.")
